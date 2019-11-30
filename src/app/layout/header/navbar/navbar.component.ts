db501c7c82c3c189260354d4f6b2bb40b322fb49
Peter Leiva <pleiva@id.uff.br>
2019-11-30 16:26:26 -0300

    WIP Feat: Navbar Component

---
+++ src/app/layout/header/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+  faMenu = faBars;
+}
---
+++ src/app/shared/interfaces/collapsible.interface.ts
@@ -0,0 +1,4 @@
+export default interface Collapsible {
+  collapse(): void;
+  expand(): void;
+}
