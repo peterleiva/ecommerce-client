0a454a30b9d13b0ad7b45dec0fe82b21a9a77783
Peter Leiva <pleiva@id.uff.br>
2019-11-30 16:23:27 -0300

    Install FontAwesome for nice icons

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  declarations: [
+
+  ],
+  exports: [
+    FontAwesomeModule
+  ]
+})
+export class SharedModule { }
