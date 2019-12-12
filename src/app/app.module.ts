b5b0ca73e3f764c06635e735579d90efe1f06862
Peter Leiva <pleiva@id.uff.br>
2019-12-12 19:54:13 -0300

    FIX: currency for correct locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import localePt from '@angular/common/locales/pt';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
 import { CoreModule } from './core/core.module';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
