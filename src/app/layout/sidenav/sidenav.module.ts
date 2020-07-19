cbb7c1c6c46785522fb7a67caa548ec07daaa7fb
Peter Leiva <pleiva@id.uff.br>
2020-07-19 16:39:59 -0300

    Add RouterModule to SidenavModule and resp. styles
    Gradient sidenav-drawer and sidenav-list is responsive using clamp
    function to calculates according to actual view width
    Animate the open state with all transition for app container using sass
    insted of angular animation

--- src/app/layout/sidenav/sidenav.module.ts
+++ src/app/layout/sidenav/sidenav.module.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
 
-import { SidenavTriggerDirective } from './sidenav-trigger/sidenav-trigger.directive';
-import { SidenavDrawerComponent } from './sidenav-drawer/sidenav-drawer.component';
-import { SidenavListComponent } from './sidenav-drawer/sidenav-list/sidenav-list.component';
-import { SidenavItemComponent } from './sidenav-drawer/sidenav-list/sidenav-item/sidenav-item.component';
 import { NavigationDataService } from './services/navigation-data.service';
+import { SidenavDrawerComponent } from './sidenav-drawer/sidenav-drawer.component';
+import { SidenavItemComponent } from './sidenav-drawer/sidenav-list/sidenav-item/sidenav-item.component';
+import { SidenavListComponent } from './sidenav-drawer/sidenav-list/sidenav-list.component';
+import { SidenavTriggerDirective } from './sidenav-trigger/sidenav-trigger.directive';
+
 
 @NgModule({
   declarations: [
@@ -15,6 +18,8 @@ import { NavigationDataService } from './services/navigation-data.service';
     SidenavItemComponent,
   ],
   imports: [
+    CommonModule,
+    RouterModule,
     SharedModule
   ],
 
