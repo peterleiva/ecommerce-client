675933808c5e5d789796d0d3185ce266932da322
Peter Leiva <pleiva@id.uff.br>
2020-07-19 20:39:03 -0300

    Remove draggable for app container and delegates trigger

--- src/app/core/app-container/app-container.component.ts
+++ src/app/core/app-container/app-container.component.ts
@@ -1,14 +1,17 @@
+/**
+ * This file describe a the whole app container
+ * @packageDocumentation
+ */
+
 import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
   Component,
-  ContentChild,
-  ElementRef,
   HostBinding,
   Input,
-  AfterViewInit,
-  OnDestroy,
-  HostListener,
-  NgZone} from '@angular/core';
-import { gsap, TweenLite, Back, Bounce } from 'gsap';
+  ViewContainerRef,
+} from '@angular/core';
+import { gsap } from 'gsap';
 import { Draggable } from 'gsap/draggable';
 
 import Menu from 'src/app/layout/sidenav/menu.interface';
@@ -16,119 +19,81 @@ import { SidenavTriggerDirective } from 'src/app/layout/sidenav/sidenav-trigger/
 
 gsap.registerPlugin(Draggable);
 
-
 // FIXME: SNAP. Em telas pequenas jogar forte para um lado e ficar preso
 
+/**
+ * Container for whole application containing srink animation logic when open
+ */
 @Component({
   selector: 'store-app-container',
   template: `<ng-content></ng-content>`,
   styleUrls: ['./app-container.component.scss'],
-  exportAs: 'storeMenu'
+  exportAs: 'storeMenu',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppContainerComponent implements AfterViewInit, Menu {
   static DRAG_ANIMATION_DURATION = .25;
   static SIDEBAR_SNAP_AXIS = 278;
 
   @HostBinding('class.open') @Input() opened: boolean;
-  @HostBinding('class.opening') private opening = false;
+  @HostBinding('class.opening') opening;
   @Input() draggable: HTMLElement;
-  @ContentChild(SidenavTriggerDirective) trigger: SidenavTriggerDirective;
+  @Input() trigger: SidenavTriggerDirective;
 
-  constructor(private container: ElementRef, private zone: NgZone) { }
+  constructor(private container: ViewContainerRef) { }
 
   private get app(): HTMLDivElement {
-    return this.container.nativeElement;
+    return this.container.element.nativeElement;
   }
 
   /**
-   *
+   * Sets the current state as opened
    */
   open(): AppContainerComponent {
-    this.zone.run(async () => {
-      this.opening = true;
-      await this.openDrawer();
-      this.opening = false;
-
-      this.opened = true;
-    });
+    this.opened = true;
 
     return this;
   }
 
   /**
-   *
+   * Sets the current state as closed
    */
   close(): AppContainerComponent {
-    this.zone.run(async () => {
-      await this.closeDrawer();
-      this.opened = false;
-    });
+    this.opened = false;
 
     return this;
   }
 
   ngAfterViewInit() {
-    Draggable.create(this.app, {
-      edgeResistance: 0,
-      trigger: this.draggable,
-      type: 'x',
-      inertia: true,
-      onDragStart: () => this.onDragStart(),
-      onDragEnd: () => this.onDragEnd()
-    });
-
-    this.opened ? this.open() : this.close();
-  }
-
-  @HostListener('click') private closeContainer(): void {
-    if (!this.opening && this.opened) {
-      this.trigger.closeNav();
-    }
+    // Draggable.create(this.app, {
+    //   edgeResistance: 0,
+    //   trigger: this.draggable,
+    //   type: 'x',
+    //   inertia: true,
+    //   onDragStart: () => this.opening = true,
+    //   onDragEnd: () => this.onDragEnd()
+    // });
   }
 
   private onDragStart(): void {
     this.opening = true;
 
-    TweenLite.to(this.app, AppContainerComponent.DRAG_ANIMATION_DURATION, {
-      rotation: 1,
-      scale: .84,
-      transformOrigin: 'left',
-      ease: Back.easeOut
-    });
+    // TweenLite.to(this.app, AppContainerComponent.DRAG_ANIMATION_DURATION, {
+    //   rotation: 1,
+    //   scale: .84,
+    //   transformOrigin: 'left',
+    //   ease: Back.easeOut
+    // });
   }
 
   private async onDragEnd(): Promise<void> {
     const closeDistance = this.app.getBoundingClientRect().left;
 
     if (closeDistance < AppContainerComponent.SIDEBAR_SNAP_AXIS) {
-      this.trigger.closeNav();
+      this.open();
     } else {
-      this.trigger.openNav();
+      this.close();
     }
     this.opening = false;
-  }
-
-  private async openDrawer(): Promise<TweenLite> {
-    return (
-      TweenLite
-        .to(this.app, AppContainerComponent.DRAG_ANIMATION_DURATION, {
-          x: AppContainerComponent.SIDEBAR_SNAP_AXIS,
-          rotation: 0,
-          scale: .84,
-          ease: Back.easeOut
-        })
-    );
-  }
-
-  private async closeDrawer(): Promise<TweenLite> {
-    return (
-      TweenLite
-        .to(this.app, AppContainerComponent.DRAG_ANIMATION_DURATION, {
-          x: 0,
-          rotation: 0,
-          scale: 1,
-          ease: Bounce.easeOut
-        })
-    );
   }
 }
