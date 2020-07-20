6e73947d17a24e2f439300d0b14d0e3b31df3de2
Peter Leiva <pleiva@id.uff.br>
2020-07-20 13:06:02 -0300

    Comment gsap code for animate sidenav-list

--- src/app/layout/sidenav/sidenav-drawer/sidenav-list/sidenav-list.component.ts
+++ src/app/layout/sidenav/sidenav-drawer/sidenav-list/sidenav-list.component.ts
@@ -2,21 +2,27 @@
  * This file is a side navigation list component
  * @packageDocumentation
  */
+
+import {
+  animate,
+  animateChild,
+  query,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
+
 import {
   AfterViewInit,
+  ChangeDetectionStrategy,
   Component,
-  ElementRef,
-  Input,
-  QueryList,
-  ViewChildren,
-  ChangeDetectionStrategy
+  Input
 } from '@angular/core';
 
-import { TimelineMax } from 'gsap';
 import { Tree } from 'src/app/shared/data-structure/tree/tree.model';
 import NavigationItem from '../../../models/navigation-item.model';
 import { SidenavNavigatorService } from '../../services/sidenav-navigator.service';
-import { trigger, style, transition, query, stagger, animate, animateChild, state } from '@angular/animations';
+
 
 /**
  * Side navegation list component
@@ -45,35 +51,11 @@ import { trigger, style, transition, query, stagger, animate, animateChild, stat
 })
 export class SidenavListComponent implements AfterViewInit {
   @Input() navList: Tree<NavigationItem>[];
-  @ViewChildren('items') private _items: QueryList<ElementRef<HTMLElement>>;
-  private timeline: TimelineMax;
 
-  constructor(private container: ElementRef,
-              private navNavigator: SidenavNavigatorService) { }
-  /**
-   * Gets the sidenav list component element for gsap
-   */
-  get nav(): HTMLElement {
-    return this.container.nativeElement;
-  }
-
-  /**
-   * Gets the sidenav underlying elements for gsap
-   */
-  get items(): HTMLElement[] {
-    // return this._items.toArray().map(item => item.nativeElement);
-  }
+  constructor(private navNavigator: SidenavNavigatorService) { }
 
   ngAfterViewInit() {
     // this._items.changes.subscribe(_ => this.openAnimation());
-  }
-
-  ngOnInit() {
-    console.log('init sidenav list');
-  }
-
-  ngOnChanges(changes) {
-    console.log('sidenavlist changed', changes);
   }
 
   /**
@@ -86,35 +68,35 @@ export class SidenavListComponent implements AfterViewInit {
   /**
    * Uses gsap to animate the hiden of old nav items
    */
-  private async closeAnimation(): Promise<TimelineMax> {
-    this.timeline = new TimelineMax();
+  // private async closeAnimation(): Promise<TimelineMax> {
+  //   this.timeline = new TimelineMax();
 
-    this.timeline
-      .set(this.nav, {
-        transformOrigin: 'center bottom',
-        perspective: 5000,
-        transformStyle: 'preserve-3d'
-      })
-      .add('close')
-      .to(this.nav, .2, { rotateX: 90 });
+  //   this.timeline
+  //     .set(this.nav, {
+  //       transformOrigin: 'center bottom',
+  //       perspective: 5000,
+  //       transformStyle: 'preserve-3d'
+  //     })
+  //     .add('close')
+  //     .to(this.nav, .2, { rotateX: 90 });
 
-    return this.timeline.then();
-  }
+  //   return this.timeline.then();
+  // }
 
   /**
    * Uses gsap to animation when the navigate event happens
    */
-  private async openAnimation(): Promise<TimelineMax> {
-    return (
-      this.timeline
-        .set(this.nav, { rotateX: 0 })
-        .set(this.items, { x: '-100%', opacity: 0 })
+  // private async openAnimation(): Promise<TimelineMax> {
+  //   return (
+  //     this.timeline
+  //       .set(this.nav, { rotateX: 0 })
+  //       .set(this.items, { x: '-100%', opacity: 0 })
 
-        .to(this.items, .25, {
-          x: 0,
-          opacity: 1,
-          stagger: { amount: .25 }
-        })
-    );
-  }
+  //       .to(this.items, .25, {
+  //         x: 0,
+  //         opacity: 1,
+  //         stagger: { amount: .25 }
+  //       })
+  //   );
+  // }
 }
