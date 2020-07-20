eda2dc63a7b3b34da77e878891b514d26023d958
Peter Leiva <pleiva@id.uff.br>
2020-07-19 21:39:06 -0300

    Remove drag feature app container and use tap to close
    Draggable is decided to be removed from app container in favor of tap to
    tap to close the container

--- src/app/core/app-container/app-container.component.ts
+++ src/app/core/app-container/app-container.component.ts
@@ -4,22 +4,14 @@
  */
 
 import {
-  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
   HostBinding,
   Input,
   ViewContainerRef,
 } from '@angular/core';
-import { gsap } from 'gsap';
-import { Draggable } from 'gsap/draggable';
 
 import Menu from 'src/app/layout/sidenav/menu.interface';
-import { SidenavTriggerDirective } from 'src/app/layout/sidenav/sidenav-trigger/sidenav-trigger.directive';
-
-gsap.registerPlugin(Draggable);
-
-// FIXME: SNAP. Em telas pequenas jogar forte para um lado e ficar preso
 
 /**
  * Container for whole application containing srink animation logic when open
@@ -31,20 +23,13 @@ gsap.registerPlugin(Draggable);
   exportAs: 'storeMenu',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppContainerComponent implements AfterViewInit, Menu {
+export class AppContainerComponent implements Menu {
   static DRAG_ANIMATION_DURATION = .25;
   static SIDEBAR_SNAP_AXIS = 278;
 
   @HostBinding('class.open') @Input() opened: boolean;
-  @HostBinding('class.opening') opening;
-  @Input() draggable: HTMLElement;
-  @Input() trigger: SidenavTriggerDirective;
 
   constructor(private container: ViewContainerRef) { }
-
-  private get app(): HTMLDivElement {
-    return this.container.element.nativeElement;
-  }
 
   /**
    * Sets the current state as opened
@@ -62,38 +47,5 @@ export class AppContainerComponent implements AfterViewInit, Menu {
     this.opened = false;
 
     return this;
-  }
-
-  ngAfterViewInit() {
-    // Draggable.create(this.app, {
-    //   edgeResistance: 0,
-    //   trigger: this.draggable,
-    //   type: 'x',
-    //   inertia: true,
-    //   onDragStart: () => this.opening = true,
-    //   onDragEnd: () => this.onDragEnd()
-    // });
-  }
-
-  private onDragStart(): void {
-    this.opening = true;
-
-    // TweenLite.to(this.app, AppContainerComponent.DRAG_ANIMATION_DURATION, {
-    //   rotation: 1,
-    //   scale: .84,
-    //   transformOrigin: 'left',
-    //   ease: Back.easeOut
-    // });
-  }
-
-  private async onDragEnd(): Promise<void> {
-    const closeDistance = this.app.getBoundingClientRect().left;
-
-    if (closeDistance < AppContainerComponent.SIDEBAR_SNAP_AXIS) {
-      this.open();
-    } else {
-      this.close();
-    }
-    this.opening = false;
   }
 }
